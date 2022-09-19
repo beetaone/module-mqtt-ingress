48515478135288ec5fb1838ae43438eabb401a52
kubagrzelak <jakubgrzelak1@gmail.com>
2022-09-19 13:45:34 +0100

    Added better handling for url protocols

--- image/src/app.js
+++ image/src/app.js
@@ -2,40 +2,63 @@ const mqtt = require('mqtt')
 const { broker, port, topic, protocol } = require('./config/broker')
 const { urls } = require('./config/nextContainer.js')
 const fetch = require('node-fetch')
+const { URL } = require('url')
 
-if(broker.includes('tcp') || broker.includes('tls') || broker.includes('ws') || broker.includes('wss') || broker.includes('wxs') || broker.includes('alis')) {
-  console.error("Unsupported url protocol. Please provide mqtt:// or mqtts:// urls.")
-} else {
-  var brokerUrl = broker;
+var brokerUrl = broker
 
-  if (!broker.includes('mqtt://') && !broker.includes('mqtts://')) {
-    brokerUrl = protocol + '://' + broker
+try {
+  brokerUrl = new URL(brokerUrl)
+
+  const supportedProtocols = ['mqtt:', 'mqtts:']
+
+  if(supportedProtocols.includes(brokerUrl.protocol)) {
+    brokerUrl = brokerUrl.href
+  } else {
+    console.error(`Unsupported url protocol (${brokerUrl.protocol}). Please provide mqtt:// or mqtts:// urls.`)
+    // kill the program
+    process.exit(1);
   }
 
-  const client = mqtt.connect(brokerUrl, {
-    port,
-    protocol,
-  })
-
-  client.on('connect', () => {
-    console.info(`Connected to broker ${brokerUrl}`)
-    client.subscribe(topic, err => err && console.error(err))
-  })
-
-  client.on('error', err => {
-    client.end()
-    console.log(err)
-  })
-
-  client.on('message', (_, body) => {
-    console.log(body.toString())
-
-    // fan-out
-    console.log(urls)
-    urls.forEach(url => {
-      fetch(url, { method: 'POST', headers: {'Content-Type': 'application/json'}, body })
-      .then(res => console.log(res))
-      .catch(e => console.error(e))
-    })
-  })
+} catch (e) {
+  if (e.code === 'ERR_INVALID_URL') {
+    if (!brokerUrl.includes("://")) {
+      // url address missing protocol so compose it basing on module envs
+      brokerUrl = protocol + '://' + broker
+    } else {
+      console.error(e)
+      // kill the program
+      process.exit(1);
+    }
+  } else {
+    console.error(e)
+    // kill the program
+    process.exit(1);
+  }
 }
+
+const client = mqtt.connect(brokerUrl, {
+  port,
+  protocol,
+})
+
+client.on('connect', () => {
+  console.info(`Connected to broker ${brokerUrl}`)
+  client.subscribe(topic, err => err && console.error(err))
+})
+
+client.on('error', err => {
+  client.end()
+  console.log(err)
+})
+
+client.on('message', (_, body) => {
+  console.log(body.toString())
+
+  // fan-out
+  console.log(urls)
+  urls.forEach(url => {
+    fetch(url, { method: 'POST', headers: {'Content-Type': 'application/json'}, body })
+    .then(res => console.log(res))
+    .catch(e => console.error(e))
+  })
+})
